b2f6dbbff1220f1b9fb96a390094d005cfc3538e
jfarrow02 <jack.dempsey.farrow@gmail.com>
2022-05-31 22:40:07 -0400

    temp refactor to dummy test spec

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,7 +1,12 @@
 
+// describe('my first test', () => {
+//     it('visits the restaurantfinder app', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.contains('Hello World');
+//     });
+// });
 describe('my first test', () => {
-    it('visits the restaurantfinder app', () => {
-        cy.visit('http://localhost:3000');
-        cy.contains('Hello World');
-    });
-});
+    it('should be true', () => {
+        expect(true).to.equal(true);
+    })
+})
