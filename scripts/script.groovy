38349044ba401c83ed6c71b0f24462605be5c08e
jfarrow02 <jack.dempsey.farrow@gmail.com>
2022-06-04 12:57:16 -0400

    adds nexus host to build script login

--- scripts/script.groovy
+++ scripts/script.groovy
@@ -6,7 +6,7 @@ def runUnitTests() {
 def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'nexus-creds', usernameVariable: 'USER', passwordVariable: 'PWD')]) {
         sh "docker build -t $IMAGE_NAME ."
-        sh "echo $PWD | docker login -u $USER --password-stdin $NEXUS_REPO_IP:$NEXUS_PORT"
+        sh "echo $PWD | docker login -u $USER --password-stdin $NEXUS_REPO_IP"
         sh "docker push $IMAGE_NAME"
     }
 }
