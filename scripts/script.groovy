7df7c09b594a3019d604843d778cce8af15ffddf
jfarrow02 <jack.dempsey.farrow@gmail.com>
2022-06-04 13:00:27 -0400

    troubleshooting nexus login

--- scripts/script.groovy
+++ scripts/script.groovy
@@ -6,7 +6,8 @@ def runUnitTests() {
 def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'nexus-creds', usernameVariable: 'USER', passwordVariable: 'PWD')]) {
         sh "docker build -t $IMAGE_NAME ."
-        sh "echo $PWD | docker login -u $USER --password-stdin $NEXUS_REPO_IP"
+        // sh "echo $PWD | docker login -u $USER --password-stdin $NEXUS_REPO_IP"
+        sh "docker login -u $USER -p $PWD $NEXUS_REPO_IP"
         sh "docker push $IMAGE_NAME"
     }
 }
