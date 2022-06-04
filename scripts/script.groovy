435f8a758d5a5b19a5bef48b6f629c1f6e66b971
jfarrow02 <jack.dempsey.farrow@gmail.com>
2022-06-04 12:44:22 -0400

    usernamePassword

--- scripts/script.groovy
+++ scripts/script.groovy
@@ -4,7 +4,7 @@ def runUnitTests() {
 }
 
 def buildImage() {
-    withCredentials(usernamePassword([credentialsId: 'nexus-creds', usernameVariable: 'USER', passwordVariable: 'PWD'])) {
+    withCredentials([usernamePassword(credentialsId: 'nexus-creds', usernameVariable: 'USER', passwordVariable: 'PWD')]) {
         sh "docker build -t $IMAGE_NAME ."
         sh "echo $PWD | docker login -u $USER --password-stdin $NEXUS_REPO_IP:$NEXUS_PORT"
         sh "docker push $IMAGE_NAME"
